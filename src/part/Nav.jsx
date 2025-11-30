c1b64adae761581c3431d697afddb2683d275374
Your Name <your_email@example.com>
2025-11-30 14:45:21 +0530

    Nav Bar

--- src/part/Nav.jsx
+++ src/part/Nav.jsx
@@ -2,10 +2,13 @@ export default function Nav() {
     return(
         <>
         <nav>
-            <h1>Repofy</h1>
-            <h1>Repofy</h1>
-            <h1>Repofy</h1>
-            <h1>Repofy</h1>
+            <h1 className="nav-title">Repofy</h1>
+            <ul className="nav-list">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <i className='bx  bx-menu-notification' id="nav-hover"></i>
         </nav>
         </>
     )
