f5d0746022929aec1b81f2ac7b974396c5866e20
Your Name <your_email@example.com>
2025-12-01 17:10:30 +0530

    About

---
+++ src/part/About.jsx
@@ -0,0 +1,14 @@
+export default function About(){
+    return(
+        <>
+        <div className="about">
+            <ul className="aboutcontent">
+                <li>About Repofy</li>
+                <li>1) We help students perfect their project reports.</li>
+                <li>2) Through our advanced checking and review process, we ensure every report is clear, well-structured, and ready for submission.</li>
+                <li>3) Our mission is simple — to make academic project review fast, accurate, and stress-free.</li>
+            </ul>
+        </div>
+        </>
+    )
+}
